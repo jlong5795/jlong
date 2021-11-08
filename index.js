1624c0f381629df154b94c5c031a708908695daf
Jason Long <jlong5795@gmail.com>
2021-11-08 00:41:16 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+console.log(`
+Jason Long, Full Stack Engineer
+
+Chicago native with a passion for problem solving, developing systems, and leaving the world better than he left it. If those are interesting, we should have a coffee.
+🐦 Twitter: https://twitter.com/jlong5795
+📖 GitHub: https://github.com/jlong5795
+
+`);
